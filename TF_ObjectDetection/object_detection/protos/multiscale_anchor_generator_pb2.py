6d9d2726101bfcaa9f0422d969f63d09c35aa347
Kshitiz Sharma <kshitizsharma38@gmail.com>
2018-03-16 16:12:02 +0530

    Update tensorflow object detection clone and minor changes

--- TF_ObjectDetection/object_detection/protos/multiscale_anchor_generator_pb2.py
+++ TF_ObjectDetection/object_detection/protos/multiscale_anchor_generator_pb2.py
@@ -18,7 +18,7 @@ _sym_db = _symbol_database.Default()
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='object_detection/protos/multiscale_anchor_generator.proto',
   package='object_detection.protos',
-  serialized_pb=_b('\n9object_detection/protos/multiscale_anchor_generator.proto\x12\x17object_detection.protos\"\x95\x01\n\x19MultiscaleAnchorGenerator\x12\x14\n\tmin_level\x18\x01 \x01(\x05:\x01\x33\x12\x14\n\tmax_level\x18\x02 \x01(\x05:\x01\x37\x12\x17\n\x0c\x61nchor_scale\x18\x03 \x01(\x02:\x01\x34\x12\x15\n\raspect_ratios\x18\x04 \x03(\x02\x12\x1c\n\x11scales_per_octave\x18\x05 \x01(\x05:\x01\x32')
+  serialized_pb=_b('\n9object_detection/protos/multiscale_anchor_generator.proto\x12\x17object_detection.protos\"\xba\x01\n\x19MultiscaleAnchorGenerator\x12\x14\n\tmin_level\x18\x01 \x01(\x05:\x01\x33\x12\x14\n\tmax_level\x18\x02 \x01(\x05:\x01\x37\x12\x17\n\x0c\x61nchor_scale\x18\x03 \x01(\x02:\x01\x34\x12\x15\n\raspect_ratios\x18\x04 \x03(\x02\x12\x1c\n\x11scales_per_octave\x18\x05 \x01(\x05:\x01\x32\x12#\n\x15normalize_coordinates\x18\x06 \x01(\x08:\x04true')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -67,6 +67,13 @@ _MULTISCALEANCHORGENERATOR = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
+    _descriptor.FieldDescriptor(
+      name='normalize_coordinates', full_name='object_detection.protos.MultiscaleAnchorGenerator.normalize_coordinates', index=5,
+      number=6, type=8, cpp_type=7, label=1,
+      has_default_value=True, default_value=True,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
   ],
   extensions=[
   ],
@@ -79,7 +86,7 @@ _MULTISCALEANCHORGENERATOR = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=87,
-  serialized_end=236,
+  serialized_end=273,
 )
 
 DESCRIPTOR.message_types_by_name['MultiscaleAnchorGenerator'] = _MULTISCALEANCHORGENERATOR
